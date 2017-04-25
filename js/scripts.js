abd4ed44753c2cfcb9195f22be1c90fb545a3dca
Andrew Glines and Olena Kuchko <andrew@>
2017-04-25 09:48:22 -0700

    add some text and change it using JQ and CSS

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,26 @@ $(document).ready(function() {
     $("#walrus-showing").slideToggle();
     $("#walrus-hidden").slideUp();
   });
+
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+  $("#orange").click(function(){
+    $("#orange").addClass("specialborder");
+  });
+  $("#blue").click(function(){
+    $("#blue").addClass("blue");
+  });
 });
